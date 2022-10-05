4174ade3d30b81007e4b49dea850c4e0e6f7b9a5
Mirea Klee <mireaklee@Mireas-MacBook-Pro.local>
2022-10-04 20:52:00 -0400

    in-class work

--- brook.js
+++ brook.js
@@ -1,7 +1,7 @@
-
 const playButton = document.querySelector('button');
 var audioCtx;
-    
+
+
 playButton.addEventListener('click', function () {
     if (!audioCtx) {
         initAudio();
@@ -16,13 +16,10 @@ playButton.addEventListener('click', function () {
 
 }, false);
 
-    
-
-
 function initAudio(){
 
     audioCtx = new (window.AudioContext || window.webkitAudioContext)
-    
+
     var bufferSize = 10 * audioCtx.sampleRate,
     noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate),
     output = noiseBuffer.getChannelData(0);
@@ -39,27 +36,24 @@ function initAudio(){
     brownNoise = audioCtx.createBufferSource();
     brownNoise.buffer = noiseBuffer;
     brownNoise.loop = true;
+    //brownNoise.start(0);
+
+    brownNoise.connect(audioCtx.destination);
     brownNoise.start(0); 
 
 
-    const input = audioCtx.createBiquadFilter()
-    const freq = audioCtx.createBiquadFilter()
-    const brook = audioCtx.createBiquadFilter()
-    
+    input = new BiquadFilterNode(brownNoise)
     input.type = 'lowpass'
-    input.frequency.setValueAtTime(400, audioCtx.currentTime);
-    
-    
-    freq.type = 'lowpass'
-    freq.frequency.setValueAtTime(14, audioCtx.currentTime);
-    
-    const friend = new ConstantSourceNode(audioCtx, {offset: 400})
-    brook.type = 'highpass'
-    brook.frequency = brownNoise.connect(freq) * friend + 500;
-    brook.Q = 0.03;
-    
+    input.frequency = 400;
 
-    brownNoise.connect(input).connect(brook).connect(audioCtx.destination);
-    
+    frequency = new BiquadFilterNode(brownNoise)
+    frequency.type = 'lowpass'
+    frequency.frequency = 14;
+
+    brook = new BiquadFilterNode(input);
+    brook.frequency = frequency;
+    brook.Q = 0.03;
+
+
 
 }
